bb4aed60f0668b900420133c7ab7c33f6b63f291
Alexseusik <alexgnibeda03@gmail.com>
2026-03-28 17:08:22 +0200

    Фікс бази (додавання бігінтежер)

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, BigInteger
 
 class Base(DeclarativeBase):
     pass
@@ -8,7 +8,7 @@ class Client(Base):
     __tablename__ = 'clients'
 
     req_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[int]
+    user_id: Mapped[int] = mapped_column(BigInteger)
     user_phone: Mapped[str] = mapped_column(String(20))
     user_name: Mapped[str] = mapped_column(String(50))
     currency: Mapped[str] = mapped_column(String(5))
